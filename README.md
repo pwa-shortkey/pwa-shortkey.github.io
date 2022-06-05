b970b7dbbe48e5ef3ddef6ebfb846699f7a4d15a
pwa-shortkey <tw.pwa.shortkey@gmail.com>
2022-06-05 10:40:32 +0800

    22

---
+++ README.md
@@ -0,0 +1,1 @@
+# pwa-gpacalculator.github.io
---
+++ sw.js
@@ -0,0 +1,51 @@
+// Cache names
+var dataCacheName = 'GPA-Cal'
+var cacheName = 'GPA_Cal'
+// Application shell files to be cached
+var filesToCache = [
+	'/',
+	'index.html',
+	'app.js',
+	'materialize.min.js',
+	'css/style.css',
+	'css/materialize.min.css',
+	'src/icon/assets/icons/icon-512x512.png'
+]
+self.addEventListener('install', function (e) {
+	console.log('[ServiceWorker] Install')
+	e.waitUntil(
+		caches.open(cacheName).then(function (cache) {
+			console.log('[ServiceWorker] Caching app shell')
+			return cache.addAll(filesToCache)
+		})
+	)
+})
+self.addEventListener('activate', function (e) {
+	console.log('[ServiceWorker] Activate')
+	e.waitUntil(
+		caches.keys().then(function (keyList) {
+			return Promise.all(keyList.map(function (key) {
+				if (key !== cacheName && key !== dataCacheName) {
+					console.log('[ServiceWorker] Removing old cache', key)
+					return caches.delete(key)
+				}
+			}))
+		})
+	)
+	return self.clients.claim()
+})
+self.addEventListener('fetch', function (e) {
+	console.log('[ServiceWorker] Fetch', e.request.url)
+	e.respondWith(
+		caches.match(e.request).then(function (response) {
+			return response || fetch(e.request)
+		})
+	)
+})
+
+
+
+
+
+
+
